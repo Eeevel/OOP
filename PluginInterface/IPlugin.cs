007ef1e95e26d76659cce2e61d1337345550a539
Pavel <pavel.shalyga.1992@gmail.com>
2021-04-01 12:31:47 +0300

    Added figures library

--- PluginInterface/IPlugin.cs
+++ PluginInterface/IPlugin.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Laba2;
 
 namespace PluginInterface
 {
     public interface IPlugin
     {
-        string Calculate();
+        string Calculate(Figure figure);
     }
 }
